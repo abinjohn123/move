700a7d5f33db686d75f1b14b99f1c2271949d496
Abin John <abinjohnanto@gmail.com>
2022-08-24 20:46:39 +0530

    feat: load leaflet map

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+class App {
+  constructor() {
+    this._getLocation();
+  }
+  _getLocation() {
+    if (!navigator.geolocation) return;
+
+    navigator.geolocation.getCurrentPosition(
+      this._loadMap.bind(this),
+      this._geolocationFailureHandler
+    );
+  }
+
+  _geolocationFailureHandler() {
+    alert('Error accessing location data');
+  }
+
+  _loadMap(geoObj) {
+    const { latitude, longitude } = geoObj.coords;
+    const position = [latitude, longitude];
+
+    this._map = L.map('map').setView(position, 18);
+    console.log(this._map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap',
+    }).addTo(this._map);
+  }
+}
+
+const app = new App();
