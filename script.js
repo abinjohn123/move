36597f7ed8c6cdf3984d67b334d1feff3d3f9c42
Abin John <abinjohnanto@gmail.com>
2022-09-01 11:51:42 +0530

    feat: implement routes and click on route to add markers in between

--- script.js
+++ script.js
@@ -38,21 +38,25 @@ class App {
       maxZoom: 19,
       attribution: '© OpenStreetMap',
     }).addTo(this._map);
+    this._initPathOverlay();
+
+    // this._routeControl = L.Routing.control({
+    //   routeWhileDragging: false,
+    //   addWaypoints: false,
+    //   // draggableWaypoints: false,
+    // }).addTo(this._map);
 
     this._map.on('click', this._addMarker.bind(this));
   }
 
   _addMarker(event) {
+    console.log('addmarker');
     const { lat, lng } = event.latlng;
     this._workout._addPoint({ lat, lng });
     this._renderMarkers(this._workout._geoPoints);
   }
 
   _renderMarkers(geoPoints) {
-    const openPopup = function () {
-      this.openPopup();
-    };
-
     const markerOptions = {
       title: 'click and drag to move',
       draggable: true,
@@ -82,18 +86,57 @@ class App {
       marker.on('click', this._removeMarker.bind(this));
       marker.on('dragend', this._updateMarker.bind(this));
     });
+    this._renderRoute();
   }
 
   _updateMarker(ev) {
+    console.log('update event');
     const index = Number.parseInt(ev.target._popup._content) - 1;
     this._workout._updatePoint(index, ev.target._latlng);
     ev.target.openPopup();
+    this._renderRoute();
   }
 
   _removeMarker(ev) {
     ev.target.removeFrom(this._map);
     this._workout._removePoint(ev.latlng);
     this._renderMarkers(this._workout._geoPoints);
+  }
+
+  _initPathOverlay() {
+    const pathOptions = {
+      color: '#3388ff',
+      weight: 8,
+      opacity: 0.8,
+    };
+
+    this._polyline = L.polyline([], pathOptions);
+    this._polyline.on('click', this._lineClickHandler.bind(this));
+  }
+
+  _lineClickHandler(ev) {
+    const linePoints = ev.target._latlngs;
+    const { lat: x, lng: y } = ev.latlng;
+
+    for (let i = 0; i < linePoints.length - 1; ++i) {
+      const { lat: x1, lng: y1 } = linePoints[i];
+      const { lat: x2, lng: y2 } = linePoints[i + 1];
+      const p1 = L.point(x1, y1);
+      const p2 = L.point(x2, y2);
+      const bounds = L.bounds(p1, p2);
+
+      if (bounds.contains(L.point(x, y))) {
+        this._workout._addPointInBetween(i + 1, { lat: x, lng: y });
+        this._renderMarkers(this._workout._geoPoints);
+        this._renderRoute();
+      }
+    }
+    L.DomEvent.stopPropagation(ev);
+  }
+
+  _renderRoute() {
+    const points = this._workout._geoPoints;
+    this._polyline.setLatLngs(points).addTo(this._map);
   }
 }
 
@@ -104,6 +147,12 @@ class Workout {
 
   _addPoint({ lat, lng }) {
     this._geoPoints.push([lat, lng]);
+  }
+
+  _addPointInBetween(index, { lat, lng }) {
+    const pointsToLeft = this._geoPoints.slice(0, index);
+    const pointsToRight = this._geoPoints.slice(index);
+    this._geoPoints = [...pointsToLeft, [lat, lng], ...pointsToRight];
   }
 
   _removePoint({ lat, lng }) {
